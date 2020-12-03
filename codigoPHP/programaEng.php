6fe275ae08e3e061510922b157d1ff4cb1334848
Miguel Angel <miguela.aragar@educa.jcyl.es>
2020-12-03 10:38:18 +0100

    Paso a producción proyectoLoginLogofTema5

---
+++ codigoPHP/programaEng.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuarioDAW210DBProyectoTema5'])) {
+    header("Location: ../login.php");
+    exit;
+}
+if (isset($_REQUEST['idioma']) && $_REQUEST['idioma'] =='esp'){
+    setcookie('idioma', 'esp');
+    header("Location: programa.php");
+    exit;
+}
+if ($_COOKIE['idioma'] == 'esp') {
+    header("Location: programa.php");
+    exit;
+}
+
+if (isset($_POST['close'])) {
+    session_destroy();
+    header("Location: ../login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Login Logoff Chaper 5</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../webroot/css/stylePrograma.css">
+        <link rel="stylesheet" type="text/css" href="../webroot/css/style_1.css">
+    </head>
+    <body>
+        <div id="cabecera">
+            <div id="titulo">
+                <h1>Login Logoff Chaper 5</h1>
+            </div>
+            <div class="nav">
+                <a href="../../../proyectoDWES/indexProyectoDWES.html" class="boton volver"><img class="icoBoton" src="../webroot/img/volver-flecha-izquierda.png"><span class="texto">Back</span></a>
+            </div>
+        </div>
+        <div id="contenedor"> 
+            <?php
+            require_once "../config/conexionBDPDO.php";
+            try {
+                $miDB = new PDO(DNS, USER, PASSWORD, CODIFICACION);
+                $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $usuarioInsertUsuario = $_SESSION['usuarioDAW210DBProyectoTema5'];
+                $fechaHoraUltimaConexionAnterior = $_SESSION['FechaHoraUltimaConexionAnterior'];
+
+
+                $consultarUsu = "SELECT DescUsuario, NumConexiones FROM Usuario WHERE CodUsuario=:CodUsuario"; //Creamos la consulta mysq
+                $datosUsuario = $miDB->prepare($consultarUsu); //Preparamos la consulta
+                $datosUsuario->bindParam(":CodUsuario", $usuarioInsertUsuario);
+                $datosUsuario->execute(); //Ejecutamos la consulta preparada
+                $oUsuario = $datosUsuario->fetchObject(); //creamos el objeto PDO de usuario
+                ?>
+                <div id="datos">
+                    <h3>Welcome <?php echo$oUsuario->DescUsuario; ?></h3>
+                    <?php
+                    if ($oUsuario->NumConexiones == 1) {
+                        echo "<h4>It is your first connection. Thank you very much for trusting us.</h4>";
+                    } else {
+                        echo "<h4>This is your " . $oUsuario->NumConexiones . " connection.</h4>";
+                        echo "<h4>His last connection was " . date('d/m/Y H:i:s', $fechaHoraUltimaConexionAnterior) . ".</h4>";
+                    }
+                } catch (PDOException $miExcepcionPDO) {
+                    echo "<div class = 'contenedorError'>";
+                    echo "<div class = 'box'>";
+                    echo "<p class = 'error'>Error " . $miExcepcionPDO->getMessage() . "</p>";
+                    echo "<p class = 'error'>Cod.Error " . $miExcepcionPDO->getCode() . "</p>";
+                    echo "<h2 class = 'error'>Failed to connect to the database</h2>";
+                    echo "</div>";
+                } finally {
+                    unset($miConexion); //cerramos la conexión
+                }
+                ?>
+                <div id="idiomas">
+                    <form  name="setIdioma" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <label for="idioma">language</label>
+                        <select id="idioma" name="idioma" onchange="this.form.submit()">              
+                            <option value="esp" <?php echo ($_COOKIE['idioma']) == 'esp' ? 'selected' : '' ?>>Spanish</option>
+                            <option value="eng" <?php echo ($_COOKIE['idioma']) == 'eng' ? 'selected' : '' ?>>English</option>
+                        </select>
+                    </form>
+                </div>
+                <?php
+                ?>
+                <div class="botones">
+                    <form  name="logout" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <button class="botonEnvio" type="submit" name='close' value="Cerrar Sesion" >Close Session</button>
+                    </form>
+                    <a href="../../proyectoTema5.html"><button class="botonEnvio">Back</button></a>
+                    <a href="detalles.php"><button class="botonEnvio">Server Details</button></a>
+                </div>    
+            </div>
+        </div>
+        <footer>
+            <div class="pie">
+                <a href="../../../index.html" class="nombre">Miguel Ángel Aranda García</a>
+                <a href="https://github.com/MiguelAranda-Sauces" class="git" ><img class="git" src="../webroot/img/git.png"></a>
+            </div>
+
+        </footer>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,140 @@
+<?php
+/*
+ * @autor: Miguel Angel Aranda Garcia <miguela.aragar@educa.jcyl.es>
+ * @version 1.0
+ * @since 29/11/2020 1.0:
+ */
+session_start();
+if (!isset($_SESSION['usuarioDAW210DBProyectoTema5'])) {
+
+    require_once '../core/201109libreriaValidacion.php'; //incluimos la libreria de validación
+
+    define("OBLIGATORIO", 1); //definimos e inicializamos la constante obligatorio a 1
+
+    $entradaOK = true; //declaramos y inicializamos la variable entradaObligatorioK, esta variable decidira si es correcta la entrada de datos del formulario
+
+    $aFormulario = [//declaramos y inicializamos el array de los campos del formulario a null
+        "usuario" => null,
+        "password" => null
+    ];
+    $aError = [//declaramos y inicializamos el array de los errores de los campos del formulario a null
+        "usuario" => null,
+        "password" => null
+    ];
+
+    if (isset($_REQUEST["entrar"])) {
+        require_once "../config/conexionBDPDO.php"; //incluimos la conexión a la BD
+        $aError["usuario"] = validacionFormularios::comprobarAlfabetico($_REQUEST["usuario"], 20, 1, OBLIGATORIO); //Validamos la entrada del formulario para el campo textfieldObligatorio siendo este alfabetico
+        //$aError["password"] = validacionFormularios::validarPassword($_REQUEST["password"], 20, 3, OBLIGATORIO); //Validamos la entrada del formulario para el campo password siendo este alfabetico de tamaño max 50 minimo 3
+
+        try {
+            $miDB = new PDO(DNS, USER, PASSWORD, CODIFICACION);
+            $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $usuarioInsertUsuario = $_REQUEST["usuario"];
+            $passInsertUsuario = $_REQUEST["password"];
+
+            $consultarLogging = "SELECT CodUsuario, Password, FechaHoraUltimaConexion FROM Usuario WHERE CodUsuario=:CodUsuario"; //Creamos la consulta mysq
+
+            $consultaLogin = $miDB->prepare($consultarLogging); //Preparamos la consulta
+            $consultaLogin->bindParam(":CodUsuario", $usuarioInsertUsuario); //Declaramos el parametro bind
+
+            $consultaLogin->execute(); //Ejecutamos la consulta preparada
+            $oUsuario = $consultaLogin->fetchObject(); //creamos el objeto PDO de usuario
+            if ($consultaLogin->rowCount() == 1) {
+                if ($oUsuario->CodUsuario == $usuarioInsertUsuario && $oUsuario->Password == hash("sha256", $usuarioInsertUsuario.$passInsertUsuario)) {//concatenamos el usuario y la password
+                    $_SESSION['usuarioDAW210DBProyectoTema5'] = $usuarioInsertUsuario; //asignamos el valor del usuario al objero de session
+                    $_SESSION['FechaHoraUltimaConexionAnterior'] = $oUsuario->FechaHoraUltimaConexion;
+                    setcookie('idioma','esp');
+                    //Actualizamos la ultima vez que se conecto con timestamp y el número de conexiones que ha echo ese usuario
+                    $actualizarLogUsu = "UPDATE Usuario SET FechaHoraUltimaConexion =" . time() . ",NumConexiones =NumConexiones + 1 WHERE CodUsuario=:CodUsuario"; //Creamos la consulta mysq
+                    $updateUsu = $miDB->prepare($actualizarLogUsu); //Preparamos la consulta
+                    $updateUsu->bindParam(":CodUsuario", $oUsuario->CodUsuario); //Declaramos el parametro bind
+                    $updateUsu->execute(); //Ejecutamos la consulta preparada
+                } else {
+                    $aError["usuario"] = "Error de credenciales";
+                }
+            } else {
+                $aError["usuario"] = "Error de credenciales";
+            }
+        } catch (PDOException $miExcepcionPDO) {
+            echo "<div class = 'contenedorError'>";
+            echo "<div class = 'box'>";
+            echo "<p class = 'error'>Error " . $miExcepcionPDO->getMessage() . "</p>";
+            echo "<p class = 'error'>Cod.Error " . $miExcepcionPDO->getCode() . "</p>";
+            echo "<h2 class = 'error'>Error en la conexión con la base de datos</h2>";
+            echo "</div>";
+        } finally {
+            unset($miConexion); //cerramos la conexión
+        }
+
+        foreach ($aError as $errores => $value) { //Recorremos todos los campos del array $aError
+            if ($value != null) { //Si algun campo de $aError tiene un valor diferente null entonces entra
+                $entradaOK = false; // asignamos el valor a false en caso de que entre
+            }
+        }
+    } else {//si el usuario no ha pulsado el boton de enviar
+        $entradaOK = false; //asignamos el valor a false ya que no se a enviado nada.
+    }
+    if ($entradaOK) {// si el valor es true entra
+        header("Location: codigoPHP/programa.php"); //redirigimos
+    } else {
+        ?>
+        <!DOCTYPE html>
+
+        <html>
+            <head>
+                <title>Login Logoff Tema 5</title>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" type="text/css" href="../webroot/css/stylelogin.css">
+                <link rel="stylesheet" type="text/css" href="../webroot/css/style_1.css">
+            </head>
+            <body>
+                <div id="cabecera">
+                    <div id="titulo">
+                        <h1>Login Logoff Tema 5</h1>
+                    </div>
+                    <div class="nav">
+                        <a href="../../proyectoDWES/indexProyectoDWES.html" class="boton volver"><img class="icoBoton" src="../webroot/img/volver-flecha-izquierda.png"><span class="texto">Volver</span></a>
+                    </div>
+                </div>
+                <div id="contenedor"> 
+                    <div id="form">
+                        <form class="descript" action= "<?php echo $_SERVER["PHP_SELF"]
+        ?>" method= "POST">
+                            <div class="campos">
+                                <label class="labelTitle" for="usuario">Usuario: </label>
+                                <input  class="inputText" type="text" name="usuario" placeholder="Introduzca el nombre del usuario">
+                            </div>
+                            <div class="campos">
+                                <label class="labelTitle" for="password">Password: </label>
+                                <input  class="inputText" type="password" name="password">
+                                        <?php echo isset($aError["password"]) || isset($aError["usuario"]) ? "<span class='error'>" . "<br>Error de credenciales</span>" : null ?>
+
+                            </div>
+                            <div class="botonSend">
+                                <input class="botonEnvio" type= "submit" value= "Entrar" name= "entrar">
+                                <input class = "botonEnvio" type = "submit" value = "Cancelar" name = "volver">
+                            </div>
+                        </form>
+
+                    </div>
+
+                    <?php
+                }
+                ?>
+            </div>
+            <footer>
+                <div class="pie">
+                    <a href="../../index.html" class="nombre">Miguel Ángel Aranda García</a>
+                    <a href="https://github.com/MiguelAranda-Sauces" class="git" ><img class="git" src="../webroot/img/git.png"></a>
+                </div>
+
+            </footer>
+        </body>
+    </html>
+    <?php
+}else{
+    header("Location: codigoPHP/programa.php"); //redirigimos
+}
+?>
